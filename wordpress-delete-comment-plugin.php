039c4de75cb2c004ccb67c51ac526b1b258d4f74
WordPress头条 <1711023576@qq.com>
2017-06-19 07:29:50 +0800

    Update wordpress-delete-comment-plugin.php
    修改表前缀bug

--- wordpress-delete-comment-plugin.php
+++ wordpress-delete-comment-plugin.php
@@ -17,35 +17,37 @@ add_action('admin_menu', 'wpdcp_admin_menu', 2);
 
 function wptoutiao_wpdcp()
 {
+	global $table_prefix;
+	$comments_table_name = $table_prefix.'comments';
 	if(isset($_POST['delete_all'])){
 		//删除所有评论
 		//global $wpdb;
-		//$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => '0' ));
-		//$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => '1' ));
-		//$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'spam' ));
-		//$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'trash' ));
+		//$wpdb->delete( $comments_table_name, array( 'comment_approved' => '0' ));
+		//$wpdb->delete( $comments_table_name, array( 'comment_approved' => '1' ));
+		//$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'spam' ));
+		//$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'trash' ));
 	}
 	else if(isset($_POST['delete_1'])){
 		//删除待审评论、垃圾评论，回收站评论
 		global $wpdb;
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => '0' ));
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'spam' ));
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'trash' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => '0' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'spam' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'trash' ));
 	}
 	else if(isset($_POST['delete_2'])){
 		//只删除待审评论
 		global $wpdb;
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => '0' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => '0' ));
 	}
 	else if(isset($_POST['delete_3'])){
 		//只删除垃圾评论
 		global $wpdb;
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'spam' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'spam' ));
 	}
 	else if(isset($_POST['delete_4'])){
 		//只删除回收站评论
 		global $wpdb;
-		$wpdb->delete( 'wptoutiao_comments', array( 'comment_approved' => 'trash' ));
+		$wpdb->delete( $comments_table_name, array( 'comment_approved' => 'trash' ));
 	}
 	?>
 	<form method="post" name="wpdcp" id="wpdcp">   
